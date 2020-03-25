939258611a861d5be7e89c1e1a85055161671539
Gabriel Vieira Barcelos <57267134+GabrielBarcelos1@users.noreply.github.com>
2020-03-25 02:09:22 -0300

    Backend Completo

---
+++ backend/src/index.js
@@ -0,0 +1,37 @@
+
+// rotas / recursos
+
+
+
+
+// metodos HTTP
+// GET: Buscar ou listar uma informação do back-end
+//POST: Criar uma informção no Back-end
+// PUT: Alterar uma informação no back-end
+// DELETE: Deletar uma informação no back-end
+
+
+
+//tipos de parametros
+//Query params: Parametros nomeados enviados na rota após o "?",(filtros,paginação)
+//Route params: Parmetros utilizados para identificar recursos
+//request body: Corpo da requisição utilizado para criar ou alterar recursos
+
+
+//SQL: mySQL, SQLite, postgresSQL,oracle.Microsoft SQL Server
+// NoSQL: MongoDB
+
+// Driver: select * from user
+//Query builder: table('user').select('*').where()
+
+const express = require ('express')
+const cors = require('cors')
+const routes = require('./routes.js')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+
+app.listen(3333)
