9dc54185cfe9703199c15f6cb8e05b9d63d20c94
gabrielBarcelos1 <gabrielvieirabarcelos02@gmail.com>
2024-02-08 22:08:38 -0300

    adjusts

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,8 +30,15 @@ const routes = require('./routes.js')
 const app = express()
 const {errors} = require('celebrate')
 
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+  credentials: true
+}))
+
 app.use(express.json())
+
 app.use(routes)
 app.use(errors())
 app.use(express.static('public'))
