821456aaf423742f0ac11efa3b20dc8fd945f8c7
gabrielBarcelos1 <gabrielvieirabarcelos02@gmail.com>
2024-02-09 00:31:00 -0300

    Adjusts

--- backend/src/app.js
+++ backend/src/app.js
@@ -31,6 +31,11 @@ const app = express()
 const {errors} = require('celebrate')
 
 app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json())
 app.use(routes)
 app.use(errors())
